e888510f22ad328e76457246dc5f21785101e096
Priyam <priyam.v@crestinfosystems.com>
2025-08-08 11:26:41 +0530

    Joining Complete

--- 03-PostgreSQL-BootCamp/17 - Joining Multiple Tables/01_Joins.sql
+++ 03-PostgreSQL-BootCamp/17 - Joining Multiple Tables/01_Joins.sql
@@ -355,3 +355,68 @@ select
 *
 from directors
 cross join actors;
+
+-- Natural joins
+-- left
+select 
+* 
+from left_products
+Natural left join right_products;
+
+-- right
+select 
+* 
+from right_products
+Natural left join left_products;
+
+-- Natural Join movies and directors
+select 
+* 
+from movies
+Natural left join directors;
+
+select 
+* 
+from movies
+Natural right join directors;
+
+-- Append tables with different columns
+-- Creating tables
+create table table2(
+	add_date date,
+	col1 int,
+	col2 int,
+	col3 int,
+	col4 int,
+	col5 int
+);
+
+create table table1(
+	add_date date,
+	col1 int,
+	col2 int,
+	col3 int
+);
+-- Inserting data
+insert into table2(add_date,col1,col2,col3,col4,col5) values
+('2020-01-01',null, 7,8,9,10),
+('2020-01-02',11,12,13,14,15),
+('2020-01-03',16,17,18,19,20);
+
+select * from table2;
+
+insert into table1(add_date,col1,col2,col3) values
+('2020-01-01',1,2,3),
+('2020-01-02',4,5,6);
+
+select * from table1;
+
+-- Combine
+select 
+	coalesce(t1.add_date, t2.add_date) as "add_date",
+	coalesce(t1.col1, t2.col1) as col1,
+	coalesce(t1.col2, t2.col2) as col2,
+	coalesce(t1.col3, t2.col3) as col3,
+	t2.col4,
+	t2.col5
+from table1 t1 full outer join table2 t2 on (t1.add_date = t2.add_date)
