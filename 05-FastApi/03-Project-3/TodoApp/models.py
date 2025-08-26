8d5d6460516e27f5f1197d8771d7b76b8273a6f4
Priyam <priyam.v@crestinfosystems.com>
2025-08-26 17:34:31 +0530

    Api Request Methods

--- 05-FastApi/03-Project-3/TodoApp/models.py
+++ 05-FastApi/03-Project-3/TodoApp/models.py
@@ -2,10 +2,10 @@ from database import Base
 from sqlalchemy import Column, Integer, String, Boolean
 
 class Todos(Base):
-    __tablename__ = 'todos'
+    __tablename__ = "todos"
 
-    id = Column(Integer, primary_key = True, index = True)
-    title = Column(String)
-    desciption = Column(String)
-    priority = Column(Integer)
-    complete = Column(Boolean, default = False)
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(100), nullable=False)
+    desciption = Column(String(255), nullable=False)  
+    priority = Column(Integer, nullable=False)
+    complete = Column(Boolean, default=False)
